1af071c939ab2364987e75ff18e9351fd17469b4
Ahmed Ez zaouia <ahmez-za@e1r10p1.1337.ma>
2022-09-25 21:34:01 +0100

    finish the ex00 in cp00

---
+++ cp00/ex00/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahmez-za <ahmez-za@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/25 21:18:42 by ahmez-za          #+#    #+#             */
+/*   Updated: 2022/09/25 21:31:20 by ahmez-za         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int ac, char **av)
+{
+	int		i, j;
+    
+    i = 1;
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			char c = toupper(av[i][j++]);
+			std::cout << c;
+		}
+		i++;
+		if (i < ac)
+			std::cout << " ";
+	}
+	std::cout << std::endl;
+	return (0);
+}
