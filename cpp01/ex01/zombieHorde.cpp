05235cf8142e82a6d1d1563fe641a6633a86fe79
Ahmed Ez zaouia <ahmez-za@e1r11p1.1337.ma>
2022-09-26 21:28:25 +0100

    fix teh zombie malloc to new

--- cpp01/ex01/zombieHorde.cpp
+++ cpp01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: ahmez-za <ahmez-za@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 00:11:52 by ahmez-za          #+#    #+#             */
-/*   Updated: 2022/09/26 00:25:15 by ahmez-za         ###   ########.fr       */
+/*   Updated: 2022/09/26 21:25:15 by ahmez-za         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ Zombie* zombieHorde( int N, std::string name )
     int i;
     
     i = 0;
-    zbms = (Zombie *)malloc(sizeof(Zombie *) * N);
+    // zbms = (Zombie *)malloc(sizeof(Zombie *) * N); 
+    zbms = new Zombie[N];
     while (i < N)
         zbms[i++].setName(name);
     
