8988ca436584dd90150c08007ca8cd11bdf13d80
Ahmed Ez zaouia <ahmez-za@e2r13p11.1337.ma>
2022-09-30 23:32:01 +0100

    ex02 in cpp02

---
+++ cpp02/ex02/Fixed.hpp
@@ -0,0 +1,55 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+# include <iostream>
+# include <stdio.h>
+# include <iomanip>
+# include <string.h>
+# include <ctime>
+# include <bitset>
+# include <cmath>
+
+class Fixed
+{
+
+  public :
+  
+  	Fixed (); 
+  	Fixed(const int value); 
+  	Fixed(const float value);
+	Fixed(const Fixed &c); 
+	~Fixed();
+
+	Fixed& operator=(const Fixed &cop);
+	bool	operator==(Fixed const &f);
+	bool	operator!=(Fixed const &f);
+	bool	operator>(Fixed const &f);
+	bool	operator>=(Fixed const &f);
+	bool	operator<(Fixed const &f);
+	bool	operator<=(Fixed const &f);
+	Fixed	operator+(Fixed const &f);
+	Fixed	operator-(Fixed const &f);
+	Fixed	operator*(Fixed const &f);
+	Fixed	operator/(Fixed const &f);
+	Fixed	&operator++(void);
+	Fixed	&operator--(void);
+	Fixed	operator++(int);
+	Fixed	operator--(int);
+
+	float toFloat(void) const;
+	int toInt(void) const;
+  	int getRawBits() const;
+  	void setRawBits(int const raw);
+
+	static Fixed 	&min(Fixed &f1, Fixed &f2);
+	static Fixed 	&max(Fixed &f1, Fixed &f2);
+	static const Fixed 	&max(Fixed const &f1, Fixed const &f2);
+	static const Fixed 	&min(Fixed const &f1, Fixed const &f2);
+
+  private :
+  	static const int fractional_bits = 8;
+  	int fixed_value;
+};
+
+std::ostream &operator<<(std::ostream &o, Fixed const &rhs);
+
+#endif 
---
+++ cpp02/ex02/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahmez-za <ahmez-za@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/30 03:45:28 by ahmez-za          #+#    #+#             */
+/*   Updated: 2022/09/30 23:16:16 by ahmez-za         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main( void ) 
+{
+    Fixed a;
+    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+    std::cout << a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << Fixed::max( a, b ) << std::endl;
+    return (0);
+}
